82cc2a89c8052719c3058d63799250b7468e2909
3dudkin@gmail.com <3dudkin@gmail.com>
2023-12-18 20:17:52 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,58 @@
 package src;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+       String s = "Hello world";
+       //LIFO -last in first out - Stack rule
+        //1
+        //2
+        //3
+         //->3
+        //->2
+        //->1
+Human tommy = new Human("Tommy", 23);
+    //human.setName("Jonny");
+        //Human jonny = tommy; - не верное копирование объекта
+        Human jonny = new Human(tommy.getName(), tommy.getAge());
+        tommy.setName("Ronny");
+        System.out.println(tommy.getName());
+        System.out.println(jonny.getName());
+        Human jack = new Human("Jack", 33);
+        Human jackCopy = new Human("Jack", 33);
+        System.out.println(jack);
+        System.out.println(jackCopy);
+        System.out.println(jack.equals(jackCopy));
+        String a = "Test";
+        String aa = "Test";
+        System.out.println(a == aa);
+        System.out.println("--------------------------");
+        int c = 15;
+        Integer cc = 15;
+        int b = 1500;
+        Integer bb = 1500;
+        System.out.println(c == cc);
+        System.out.println(b == bb);
+        System.out.println("--------------------------");
+        String ss = "Test";
+        ss = ss + " New value";
+        System.out.println(ss);
+        String d = "123";
+        Integer f = 123;
+        Integer d1 = Integer.parseInt(d);
+        System.out.println(d1 + 1);
+
+        String test2 = String.valueOf(f);
+        System.out.println(test2 + 1);
+        String input = new Scanner(System.in).nextLine();
+        System.out.println("Enter number");
+        int lastNum = Integer.parseInt(String.valueOf(input.charAt(input.length() - 1)));
+        System.out.println(lastNum);
+
+
     }
 }
